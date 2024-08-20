6b270d62a572d738402552e6adeb0553fd6119b7
Bharat <kadamb208@gmail.com>
2024-08-20 18:14:14 +0530

    adding files switch button

--- Projects-All/src/ToggleSwitch/ToggleSwitch.jsx
+++ Projects-All/src/ToggleSwitch/ToggleSwitch.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { IoIosSwitch } from "react-icons/io";
 import './ToggleSwitch.css'
 const ToggleSwitch = () => {
+
 
     const [isOn ,setIsOn]=useState(true)
     const handleToggleSwitch=()=>{
@@ -9,12 +11,16 @@ const ToggleSwitch = () => {
     const checkIsOn=isOn ? "on" : "off"
        const toggleBGColor={background: isOn ? '#4caf50' : "#f44335"}
   return (
+    <>
+    <h1 style={{color:'#000', textAlign:'center'}}>ToggleSwitch <IoIosSwitch/></h1>
     <div className='toggle-switch' style={toggleBGColor} onClick={handleToggleSwitch}>
+        
         <div className={`switch ${checkIsOn}`}>
             <span className='switch-state'>{checkIsOn}</span>
         </div>
      
     </div>
+    </>
   )
 }
 
