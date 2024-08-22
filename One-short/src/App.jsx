ce36e8749982789c946d5bbfb120659e8e8293b5
Bharat <kadamb208@gmail.com>
2024-08-22 19:42:24 +0530

    add fiels for Routing

--- One-short/src/App.jsx
+++ One-short/src/App.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
-import Header from './componets/Header.jsx';
+import React from 'react';
+// import Header from './componets/Header.jsx';
+import Routing from './componets/Routing.jsx';
 const App = () => {
-  // const [marks, setMarks] = useState(12);
+  // const [marks, setMarks] = useState("Bharat");
+  // const [first, setFirst] = useState("Suraj")
   return (
     <>
       {/* <h1>My marks were {marks}</h1>
       <button onClick={() => setMarks(marks+1)}>Update</button> */}
-      <Header/>
+      
+      {/* <Header data={marks} data1={first}/> */}
+      {/* <h2>{marks}</h2> */}
+
+      {/* routing */}
+      <Routing/>
+
     </>
   );
 }
 
 export default App;
-//
+//  
--- One-short/src/componets/Header.jsx
+++ One-short/src/componets/Header.jsx
@@ -1,9 +1,20 @@
+'use clint'
 import React from 'react'
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
-      <h1>hello header</h1> 
+      <div className='h-16 px-5   bg-slate-500 flex items-center justify-between'> 
+      <h2>{props.data}</h2>
+      <div className='flex gap-8'>
+        <h4>{props.data1}</h4>
+        <h4>Career</h4>
+
+        <h4>Blog</h4>
+        <h4>Account</h4>
+        
+      </div>
+      </div>
     </>
   )
 }
