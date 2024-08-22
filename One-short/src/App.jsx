d28772b3c469964582e1ef199b94d98e93846933
Bharat <kadamb208@gmail.com>
2024-08-22 14:41:43 +0530

    adding files for componets

--- One-short/src/App.jsx
+++ One-short/src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { useState } from 'react'
-import Componets from './Componets.jsx';
-
+import React, { useState } from 'react';
+import Header from './componets/Header.jsx';
 const App = () => {
-var [value,setValue]=  useState(0);
+  // const [marks, setMarks] = useState(12);
   return (
-    <div>
-      <h1>hello world</h1>
-      <h4>{value}</h4>
-
-      <button onClick={()=>(setValue(value+1))} className='px-3 py-1 bg-green-500 rounded-md text-xs'>click</button>
-      <Componets age={value} data={{age:23,name:'Bharat'}}/>
-    </div>
-  )
+    <>
+      {/* <h1>My marks were {marks}</h1>
+      <button onClick={() => setMarks(marks+1)}>Update</button> */}
+      <Header/>
+    </>
+  );
 }
 
-export default App
+export default App;
+//
