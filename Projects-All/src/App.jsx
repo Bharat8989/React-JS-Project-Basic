d74ed95dc14c6db08d75f8167ae9cfe97d807cb2
Bharat <kadamb208@gmail.com>
2024-08-26 00:03:03 +0530

    adding todolist

--- Projects-All/src/App.jsx
+++ Projects-All/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import  ToggleSwitch  from './ToggleSwitch/ToggleSwitch.jsx'
-import Todo from './Todo/Todo.jsx'
+import Script from './practices/Script.jsx'
+// import  ToggleSwitch  from './ToggleSwitch/ToggleSwitch.jsx'
+// import Todo from './Todo/Todo.jsx'
 const App = () => {
-  console.log("hello world")
+  
   return (
     // <section className='container'>
     //   <ToggleSwitch/>
     // </section>
-   <section> <Todo/></section>
+  //  <section> <Todo/></section>
+  
+  <Script/>
   )
+
 }
 
 export default App
