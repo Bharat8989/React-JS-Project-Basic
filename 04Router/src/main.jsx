dc291416de5f1807ad273679cb66d86e5d90c6d8
Bharat <kadamb208@gmail.com>
2024-08-27 01:06:22 +0530

    adding routing files

---
+++ 04Router/src/main.jsx
@@ -0,0 +1,71 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Layout from './components/Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+// import { createElement } from 'react';
+import Github ,{githubInfoLoader} from './components/Github/Github.jsx';
+import User from './components/User/User.jsx';
+// import githubInfoLoader from './components/Github/Github.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />, 
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'github',
+        element: <Github />,
+        loader: githubInfoLoader
+      },
+      // {
+      //   loader:githubInfoLoader,
+      //   path: 'user/:userid',
+      //   element: <User />,
+      // },
+      {
+        path: 'user/:userId',
+        element: <User />,
+       
+      }
+      
+    ],
+  },
+]);
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout />}>
+//       <Route index element={<Home />} />
+//       <Route path='about' element={<About />} />
+//       <Route path='contact' element={<Contact />} />
+//       {/* Uncomment this route if the Github component and loader are defined */}
+//       {/* <Route 
+//         loader={githubInfoLoader}
+//         path='github' 
+//         element={<Github />}
+//       /> */}
+//     </Route>
+//   )
+// )
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
