ac0c3418563cf2f650685d6347502ab81472a073
Bharat <kadamb208@gmail.com>
2024-08-30 21:20:30 +0530

    react all files add

--- currency/src/App.jsx
+++ currency/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+// import UserContext from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login.jsx'
+import Profile from './components/Profile.jsx'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-rose-500'>hello world</h1>
-    </div>
+    <UserContextProvider>
+      {/* <h1 className='bg-rose-500'>hello world</h1> */}
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
   )
 }
 
