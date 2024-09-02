1c3859fd0b0b584244a84a3844350d120df9594c
Bharat <kadamb208@gmail.com>
2024-09-02 18:36:46 +0530

    adding files

--- Projects-All/src/App.jsx
+++ Projects-All/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Script from './practices/Script.jsx'
+// import Script from './practices/Script.jsx'
 // import  ToggleSwitch  from './ToggleSwitch/ToggleSwitch.jsx'
-// import Todo from './Todo/Todo.jsx'
+import Todo from './Todo/Todo.jsx'
 const App = () => {
   
   return (
     // <section className='container'>
     //   <ToggleSwitch/>
     // </section>
-  //  <section> <Todo/></section>
+   <section> <Todo/></section>
   
-  <Script/>
+  
   )
 
 }
