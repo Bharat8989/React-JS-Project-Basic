7343a21a39f9289379b85d3de9fdb675386b0400
Bharat <kadamb208@gmail.com>
2024-09-08 14:44:26 +0530

    adding todo list

--- Redux-toolkit/src/App.jsx
+++ Redux-toolkit/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 const App = () => {
   return (
-    <div className='text-black'>Redux-toolkit</div>
+    <>
+    <h1>Learn about redux toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+    
   )
 }
 
